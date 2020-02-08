c529851bf4e834217b306410b615b61505d2d8dc
Fernando Callejon <fernando.callejon@dr.local>
2020-02-08 19:07:39 +0000

    (wip) adding arbitrage endpoint

---
+++ README.md
@@ -0,0 +1,15 @@
+# Peregrine Arbitrage API.
+
+## Install requirements
+
+`python -m module pip install -r ./requirements.txt`
+
+## Run
+
+`python server.py`
+
+## Command line arguments
+
+1. `--port`: to specify in which port we want to run the app. [default 5002]
+2. `--isDebug`: to tell Flask whether to run in debug mode or not. [default True]
+3. `--reload`: to tell Flask whether to run with auto-reload or not. [default True]
--- server.py
+++ server.py
@@ -1,67 +1,77 @@
+import argparse
 import asyncio
 import math
 from json import dumps
 
-from flask import Flask, Response, request
+from flask import Flask, Response, request, jsonify
 from flask_restful import Api, Resource
 from peregrinearb import (bellman_ford_multi,
                           create_weighted_multi_exchange_digraph)
 
+parser = argparse.ArgumentParser(description="Peregrine Arbitrage API")
+parser.add_argument("--debug", type=bool, default=True)
+parser.add_argument("--port", type=int, default=5002)
+parser.add_argument("--reloader", type=bool, default=True)
+args = parser.parse_args();
+
 app = Flask(__name__)
 api = Api(app)
 
-class arbitrage(Resource):
-    def get_set_event_loop(self):
-        try:
+def formatToJson(graph, path, initialMoney):
+    if (path  == None):
+        return []
+
+    steps = []
+    money = initialMoney;
+    for i in range(len(path)):
+        if i + 1 < len(path):
+            start = path[i]
+            end = path[i + 1]
+            rate = math.exp(-graph[start][end]['weight'])
+            money = rate
+            step = {
+                'from': start,
+                'to': end,
+                'at': rate,
+                'total': money,
+                'exchange': graph[start][end]['exchange_name'],
+                'market': graph[start][end]['market_name']
+            }
+            steps.append(step)
+    return steps
+
+def get_set_event_loop():
+    try:
+        return asyncio.get_event_loop()
+    except RuntimeError as e:
+        if e.args[0].startswith('There is no current event loop'):
+            asyncio.set_event_loop(asyncio.new_event_loop())
             return asyncio.get_event_loop()
-        except RuntimeError as e:
-            if e.args[0].startswith('There is no current event loop'):
-                asyncio.set_event_loop(asyncio.new_event_loop())
-                return asyncio.get_event_loop()
-            raise e
+        raise e
 
-    def formatToJson(self, graph, path, initialMoney):
-        if (path  == None):
-            return []
+@app.route('/arbitrage/', defaults={'exchanges': 'binance,kraken,bittrex', 'start_coin': "BTC" })
+@app.route('/arbitrage/<string:exchanges>/<string:start_coin>')
+def get_arbitrage(exchanges, start_coin):
+    splitted_exchanges = str.split(exchanges, ',')
 
-        steps = []
-        money = initialMoney;
-        for i in range(len(path)):
-            if i + 1 < len(path):
-                start = path[i]
-                end = path[i + 1]
-                rate = math.exp(-graph[start][end]['weight'])
-                money = rate
-                step = {
-                    'from': start,
-                    'to': end,
-                    'at': rate,
-                    'total': money,
-                    'exchange': graph[start][end]['exchange_name'],
-                    'market': graph[start][end]['market_name']
-                }
-                steps.append(step)
-        return steps
+    print("Get Arbitrage called with:")
+    print(f"\tstart_coin    = '{start_coin}'")
+    print(f"\texchanges     = '{splitted_exchanges}'")
 
-    def get_arbitrage(self):
-
+    try:
         # Get posibilities
-        graph = create_weighted_multi_exchange_digraph(['binance'], log=True)
+        get_set_event_loop();
+        graph = create_weighted_multi_exchange_digraph(splitted_exchanges, log=True, fees=True)
         # search paths
-        graph, paths = bellman_ford_multi(graph, 'BTC')
-
-        steps = []
+        graph, paths = bellman_ford_multi(graph, start_coin)
+        legs = []
         for path in paths:
-            current = self.formatToJson(graph, path, 0.01482684)
-            if (current != None):
-                steps.extend(current)
+            current = formatToJson(graph, path, 0.01482684)
+            if (current != None and current.__len__() > 0):
+                legs.append(current)
 
-        return steps
+        return jsonify({'legs': legs}), 200
+    except RuntimeError as re:
+        return re.args[0], 500
 
-    def get(self):
-        self.get_set_event_loop()
-        steps = self.get_arbitrage()
-        return steps
-
-api.add_resource(arbitrage, '/arbitrage')
-app.run(debug=True, use_reloader=True, port='5002')
+app.run(debug=args.debug, use_reloader=args.reloader, port=args.port)
